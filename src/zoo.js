4ad31fa148679803d0b445fcf265db227d5c7227
BrianCaetano <brian-caetano@hotmail.com>
2021-08-04 18:40:21 -0300

    requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,7 @@
 const { species, employees } = require('./data');
 const data = require('./data');
 
-// Ajuda de Biza e Martin para fazer os valores dentro do parametro ids serem retornados (ids.soeme)
+// Ajuda de Biza e Martin para fazer os valores dentro do parametro ids serem retornados (ids.some)
 function getSpeciesByIds(...ids) {
   return species.filter((animal) => ids.some((id) => animal.id === id));
 }
@@ -25,9 +25,26 @@ function getEmployeeByName(employeeName) {
   return {};
 }
 
+// Ajuda do Martin e Daniel para usar o object destruction.
 function createEmployee(personalInfo, associatedWith) {
-
+  return { ...personalInfo, ...associatedWith };
+  // return console.log(Object.assign(personalInfo, associatedWith));
 }
+createEmployee({
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+}, {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992',
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+  ],
+});
 
 function isManager(id) {
   // seu código aqui
